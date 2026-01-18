d2da4aa7241fd1a8ec159ef3cf979939270b5ebd
sujithprakash15 <sujithprakash15@gmail.com>
2026-01-18 19:11:54 +0530

    added temp

--- Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
+++ Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5eb2f47f73a0ec11f7a2ace100e9b3b21928bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f40421ce59760520a628d8333610ef36d97ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
