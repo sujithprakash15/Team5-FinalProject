5b4cf3c527bbbc0410fc31562db9c48ab11b9b0c
Nivedhitha <nive17693@gmail.com>
2026-01-19 17:34:52 +0530

    temps

--- Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/obj/Debug/net10.0/TicketingLibrary.AssemblyInfo.cs
+++ Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/obj/Debug/net10.0/TicketingLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketingLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7ffb6f6ef2eafdbc5630406e19e84847464863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a76d4940f4be49332c49364547ccf7ccfab2cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketingLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketingLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
