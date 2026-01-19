721bde9f0eac7a4d58f59f7118535c7163a43dcf
Sri harini <harinimeckk@gmail.com>
2026-01-19 11:04:26 +0530

    employee and department added

--- Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Interfaces/IDepartmentRepository.cs
+++ Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Interfaces/IDepartmentRepository.cs
@@ -1,8 +1,14 @@
 using System;
+using TicketingLibrary.Models;
 
 namespace TicketingLibrary.Interfaces;
 
 public interface IDepartmentRepository
 {
-
+    Task AddDepartmentAsync(Department department);
+    Task<List<Department>> GetAllDepartmentsAsync();
+    Task<Department> GetDepartmentByIdAsync(int departmentId);   
+    Task UpdateDepartmentAsync(int departmentId, Department department);
+    Task DeleteDepartmentAsync(int departmentId);
 }
+
