e57c5e8b33f71bb54ca9c81b87e54331b8802ca1
Nivedhitha <nive17693@gmail.com>
2026-01-19 11:53:37 +0530

    sla added

--- Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Interfaces/ISLARepository.cs
+++ Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Interfaces/ISLARepository.cs
@@ -1,8 +1,14 @@
 using System;
+using TicketingLibrary.Models;
 
 namespace TicketingLibrary.Interfaces;
 
 public interface ISLARepository
 {
+    Task<List<SLA>> GetAllSLAsAsync();
+    Task<SLA> GetSLAAsync(string slaId);
+    Task AddSLAAsync (SLA sla);
+    Task UpdateSLAAsync (string slaId, SLA sla);
+    Task DeleteSLAAsync (string slaId);
 
 }
