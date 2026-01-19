1efc72dfeb3fffd0add1e42376324f762c9c477c
Sri harini <harinimeckk@gmail.com>
2026-01-19 14:26:51 +0530

    ticket repo added

--- Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Repos/EFEmployeeRepository.cs
+++ Ticketing_Portal_Project/Backend/Libraries/TicketingLibrary/Repos/EFEmployeeRepository.cs
@@ -1,9 +1,33 @@
 using System;
 using TicketingLibrary.Interfaces;
+using TicketingLibrary.Models;
 
 namespace TicketingLibrary.Repos;
 
 public class EFEmployeeRepository : IEmployeeRepository
 {
+    public Task AddEmployeeAsync(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task DeleteEmployeeAsync(int empId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Employee>> GetAllEmployeesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Employee> GetEmployeeByIdAsync(int empId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateEmployeeAsync(int empId, Employee employee)
+    {
+        throw new NotImplementedException();
+    }
 }
