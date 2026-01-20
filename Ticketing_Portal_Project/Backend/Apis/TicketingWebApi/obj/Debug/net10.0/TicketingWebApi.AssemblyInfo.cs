603b699bd37f6aab05b049e8e955e90abe79b4e4
Nivedhitha <nive17693@gmail.com>
2026-01-20 17:32:49 +0530

    changed

--- Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
+++ Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d12835bf4ca0e50ef71252ff0e2abce63f7535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4bb962cd629232d81a8f99e6e58d3cb54cb55d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
