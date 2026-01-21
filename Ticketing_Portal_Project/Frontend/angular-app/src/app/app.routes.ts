eb76ba5897bead99f202a98faba2680b1498044c
Sri harini <harinimeckk@gmail.com>
2026-01-21 17:18:24 +0530

    new

--- Ticketing_Portal_Project/Frontend/angular-app/src/app/app.routes.ts
+++ Ticketing_Portal_Project/Frontend/angular-app/src/app/app.routes.ts
@@ -5,39 +5,20 @@ import { RegisterComponent } from './register-component/register-component';
 import { LogoutComponent } from './logout-component/logout-component';
 import { TicketComponent } from './ticket-component/ticket-component';
 import { DepartmentComponent } from './department-component/department-component';
-<<<<<<< HEAD
 import { TicketreplyComponent } from './ticketreply-component/ticketreply-component';
 import { SlaComponent } from './sla-component/sla-component';
 import { TickettypeComponent } from './tickettype-component/tickettype-component';
 import { EmployeeComponent } from './employee-component/employee-component';
 import { NavbarComponent } from './navbar-component/navbar-component';
-=======
-<<<<<<< HEAD
-import { TicketreplyComponent } from './ticketreply-component/ticketreply-component';
-import { SlaComponent } from './sla-component/sla-component';
-=======
-import { EmployeeComponent } from './employee-component/employee-component';
->>>>>>> 5175a898bde049a9286ccf638fa0c9dae124cdd1
->>>>>>> 9a5b22ff07c5a2900be2c85b222d87c9b477e1d9
 
 export const routes: Routes = [
      {path: 'login', component: LoginComponent },
      {path: 'logout', component: LogoutComponent },
      {path: 'ticket', component: TicketComponent },
      {path: 'department', component: DepartmentComponent },
-<<<<<<< HEAD
      {path: 'ticketreply', component: TicketreplyComponent },
      {path: 'sla', component: SlaComponent },
      {path: 'tickettype', component: TickettypeComponent },
      {path: 'employee', component: EmployeeComponent },
      {path: 'navbar', component: NavbarComponent }
-
-=======
-<<<<<<< HEAD
-     {path: 'ticketreply',component: TicketreplyComponent },
-     {path: 'sla', component: SlaComponent }// Placeholder for SLA component
-=======
-     {path: 'employee', component: EmployeeComponent}
->>>>>>> 5175a898bde049a9286ccf638fa0c9dae124cdd1
->>>>>>> 9a5b22ff07c5a2900be2c85b222d87c9b477e1d9
 ];
