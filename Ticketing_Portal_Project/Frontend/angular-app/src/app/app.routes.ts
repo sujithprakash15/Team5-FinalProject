3ea7405005eaefdedecf8c291a727464c938f7cf
Dinesh Kumar.S <parishun3@gmail.com>
2026-01-21 16:30:34 +0530

    ticket reply css updated

--- Ticketing_Portal_Project/Frontend/angular-app/src/app/app.routes.ts
+++ Ticketing_Portal_Project/Frontend/angular-app/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './login-component/login-component';
 import { LogoutComponent } from './logout-component/logout-component';
 import { TicketComponent } from './ticket-component/ticket-component';
 import { DepartmentComponent } from './department-component/department-component';
+import { TicketreplyComponent } from './ticketreply-component/ticketreply-component';
+import { SlaComponent } from './sla-component/sla-component';
 
 export const routes: Routes = [
      {path: 'login', component: LoginComponent },
      {path: 'logout', component: LogoutComponent },
      {path: 'ticket', component: TicketComponent },
-     {path: 'department', component: DepartmentComponent }
+     {path: 'department', component: DepartmentComponent },
+     {path: 'ticketreply',component: TicketreplyComponent },
+     {path: 'sla', component: SlaComponent }// Placeholder for SLA component
 ];
