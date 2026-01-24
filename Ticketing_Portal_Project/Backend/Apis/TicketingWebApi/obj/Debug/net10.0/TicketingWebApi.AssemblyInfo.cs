44409560251195e017c299e49e1d9687c5a3793b
sujithprakash15 <sujithprakash15@gmail.com>
2026-01-24 13:19:56 +0530

    eod

--- Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
+++ Ticketing_Portal_Project/Backend/Apis/TicketingWebApi/obj/Debug/net10.0/TicketingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce56be00c678c0a4084aef95a04bd0ea857a5605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e063b43aa9a491b293ef0d439f170697c944eda0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
